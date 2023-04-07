4aa4393e84df5e7768fa1702eae687621566a163
Dima <d.klychnikov@petrosoft.su>
2023-04-07 16:45:54 +0300

    some

--- src/App.vue
+++ src/App.vue
@@ -24,24 +24,15 @@ export default {
       map: null,
       layer: null,
       layerZones: {
-        id: '2bd50162-47f0-45b6-9546-7c4edb4e0d16',
-        name: 'Зоны дд',
-        layers: 'fpd:l_isogd_documents_without_inzh',
-        url: '/geoserver/ows',
-        type: 10,
-        itemType: 'layer',
-        visible: true,
-        children: [],
-        style: {},
-        cqlFilter: null
+        url: '/geoserver/wms',
       },
       params: {
-        LAYERS: 'fpd:l_isogd_documents_without_inzh',
+        LAYERS: 'fpd:rosreestr_realty',
         VERSION: '1.3.0'
       },
     }
   },
-  mounted() {
+  async mounted() {
     const layers = new TileLayer({
       source: new OSM()
     })
@@ -57,29 +48,38 @@ export default {
       target: 'simple-map',
       view: view
     })
+
     let layer = this.getMapLayer()
     this.map.addLayer(layer)
+    // this.map.addLayer(await this.sendRequest())
   },
   watch: {
 
   },
   methods: {
     getMapLayer() {
+      const opacity = 1
+      const visible = true
       let mapLayer = new Image({
         source: new ImageWMS({
-          url: this.layerZones.url,
-          params: this.params,
+          url: '/geoserver/wms',
+          params: {
+            LAYERS: 'fpd:l_isogd_documents',
+            VERSION: '1.3.0'
+          },
           crossOrigin: 'anonymous',
           imageLoadFunction: undefined
-        })
+        }),
+        opacity,
+        visible
       })
-      console.log('mapLayer', mapLayer)
       return mapLayer
     },
     async sendRequest() {
       // axios.get('/geoserver/ows?service=WMS&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=fpd%3Aall_boundaries_oktmo')
-      axios.get('/geoserver/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=fpd%3Al_isogd_documents&CRS=EPSG%3A3857&STYLES=&WIDTH=894&HEIGHT=1254&BBOX=2965756.6974648386%2C7691399.534167575%2C4059111.9500559997%2C9225032.069681352')
-        .then(res => console.log(res))
+      const { data: result } = await axios.get('/geoserver/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=fpd%3Al_isogd_documents&CRS=EPSG%3A3857&STYLES=&WIDTH=894&HEIGHT=1254&BBOX=2965756.6974648386%2C7691399.534167575%2C4059111.9500559997%2C9225032.069681352')
+      return result 
+      // this.map.addLayer(layer)
     }
   }
 }
